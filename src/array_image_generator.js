57866e20479b362fc38fc10b0386490fffb52c3f
Paola Villarreal <paw@paw.mx>
2017-09-26 00:30:14 -0400

    Added a width parameter to the text so it can FitToBoundingBox.. nice little hack

--- src/array_image_generator.js
+++ src/array_image_generator.js
@@ -30,7 +30,6 @@ Array_Image_Generator.prototype.render = function (step) {
 	var me = this;
 	for (var r in matrix) {
 		var row = matrix [r], imgFile = getTemplate (row); 
-		console.log ("Opening template: " + imgFile);
 		if (fs.existsSync (imgFile)) {
 			gd.openFile (imgFile, (err, img) => {
 				for (var cl in me._colors) { 
@@ -44,9 +43,19 @@ Array_Image_Generator.prototype.render = function (step) {
 							color = (conf.color ? (typeof conf.color === "function" ? conf.color (row [cnf], row) : conf.color ) : 'white' ),
 							font = (conf.font ? (typeof conf.font === "function" ? conf.font (row [cnf], row) : conf.font ) : 'regular' ),
 							size = (conf.size ? (typeof conf.size === "function" ? conf.size (row [cnf], row) : conf.size ) : 25 ),
+							width = (conf.width ? (typeof conf.width === "function" ? conf.width (row [cnf], row) : conf.width ) : 0 ),
 							x = (conf.x ? (typeof conf.x === "function" ? conf.x (row [cnf], row) : conf.x ) : 0 ),
 							y = (conf.y ? (typeof conf.y === "function" ? conf.y (row [cnf], row) : conf.y ) : 0 )
 
+						var getSizeToFit = (txt, width, size) => { 
+							bboxWidth = 0; 
+							do {
+								[ll_x, ll_y, lr_x, lr_y, ur_x, ur_y, ul_x, ul_y] = img.stringFTBBox (colors [color], me._fonts [font], size, 0, parseInt (x), parseInt (y), txt);
+								bboxWidth = lr_x - ll_x;
+								size--;
+							} while (bboxWidth > width);
+							return size;
+						}
 						var lines = text.split ("\n");
 						if (conf.multiline) { 
 							for (var l in lines) { 
@@ -54,12 +63,12 @@ Array_Image_Generator.prototype.render = function (step) {
 								var groups = words.map ( (e, i) => { return i % max_words === 0 ? words.slice (i, i + max_words) : null }).filter ((e) => {return e;}) 
 
 								for (var g in groups ) {
-									img.stringFT (colors [color], me._fonts [font], size, 0, x, y, groups [g].join (' ') );
+									img.stringFT (colors [color], me._fonts [font], width > 0 ? getSizeToFit (groups [g].join (' '), width, size) : size, 0, parseInt (x), parseInt (y), groups [g].join (' ') );
 									y += size + (size * .25);
 								}
 							}
 						} else { 
-							img.stringFT (colors [color], me._fonts [font], size, 0, parseInt (x), parseInt (y), text);
+							img.stringFT (colors [color], me._fonts [font], width > 0 ? getSizeToFit (text, width, size) : size, 0, parseInt (x), parseInt (y), text);
 						}
 					}
 
